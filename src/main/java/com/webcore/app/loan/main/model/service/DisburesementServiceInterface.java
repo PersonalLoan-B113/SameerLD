b048544c61905e33ca5c0498b1a055cd5c07dd85
mogli4691 <mahendradhobe4@gmail.com>
2020-02-20 14:38:55 +0530

    dele

--- src/main/java/com/webcore/app/loan/main/model/service/DisburesementServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.webcore.app.loan.main.model.service;
-
-import com.webcore.app.loan.main.model.EmiDetails;
-
-public interface DisburesementServiceInterface {
-
-	EmiDetails savedata(EmiDetails em);
-
-}
